49fffadf43665543e7d55b62d8b6efed0624473d
Oliver Smith <oliver@kfs.org>
2014-09-03 17:05:31 -0700

    Made it so you can list avoidances in a comma-separated list, e.g. --avoid chango,gold,anderson

--- trade.py
+++ trade.py
@@ -206,7 +206,10 @@ def parseAvoids(avoidances):
     global avoidItems, avoidSystems, avoidStations
 
     # You can use --avoid to specify an item, system or station.
-    for avoid in avoidances:
+    # and you can group them together with commas or list them
+    # individually.
+    for avoid in ','.join(avoidances).split(','):
+        print(avoid)
         # Is it an item?
         item, system, station = None, None, None
         try:
