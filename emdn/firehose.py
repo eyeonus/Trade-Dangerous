214e91ddd96c79474f7bc4c7920fba3f37d6e3e5
Oliver Smith <oliver@kfs.org>
2014-09-04 21:37:04 -0700

    Firehose.read had lost its ability to be non-blocking. Now it found it, and you won't believe where it stuck it!

--- emdn/firehose.py
+++ emdn/firehose.py
@@ -8,29 +8,29 @@
 # Elite Market Data Net :: Modules :: Main Module
 
 """
-	"Elite Market Data Net" (EMDN) is a ZeroMQ based service that
-	provides a near-realtime feed of market scrapes from the Elite
-	Dangerous universe. This feed is called the "firehose".
+    "Elite Market Data Net" (EMDN) is a ZeroMQ based service that
+    provides a near-realtime feed of market scrapes from the Elite
+    Dangerous universe. This feed is called the "firehose".
 
-	emdn.ItemRecord class encapsulates a record as described by
-	the EMDN network.
+    emdn.ItemRecord class encapsulates a record as described by
+    the EMDN network.
 
-	emdn.Firehose class encapsulates a connection to retrieve
-	ItemRecords in an iterative fashion.
+    emdn.Firehose class encapsulates a connection to retrieve
+    ItemRecords in an iterative fashion.
 
-	Example:
+    Example:
 
-	  from emdn.firehose import Firehose
-	  firehose = Firehose()
-	  # use firehose = Firehose(ctx=ctx) if you have your own zmq.Context
+      from emdn.firehose import Firehose
+      firehose = Firehose()
+      # use firehose = Firehose(ctx=ctx) if you have your own zmq.Context
 
-	  # wait upto 10 seconds and retrieve upto 2 records:
-	  for itemRec in firehose.drink(records=2, timeout=10.0)
-	  	pass
+      # wait upto 10 seconds and retrieve upto 2 records:
+      for itemRec in firehose.drink(records=2, timeout=10.0)
+          pass
 
-	  # print everything else we receive
-	  for itemRec in firehose.drink():
-	  	print(itemRec)
+      # print everything else we receive
+      for itemRec in firehose.drink():
+          print(itemRec)
 """
 
 try:
@@ -44,90 +44,93 @@ try: from itemrecord import ItemRecord
 except ImportError: from . itemrecord import ItemRecord
 
 class Firehose(object):
-	"""
-		Encapsulates a connection to the Elite Market Data Network (EMDN)
-		live feed of price updates.
-		Also handles "file:///" URIs for local testing, e.g.
-			hose = Firehose("file://./emdn.csv")
-		reads from a local file called 'emdn.csv'.
-	"""
+    """
+        Encapsulates a connection to the Elite Market Data Network (EMDN)
+        live feed of price updates.
+        Also handles "file:///" URIs for local testing, e.g.
+            hose = Firehose("file://./emdn.csv")
+        reads from a local file called 'emdn.csv'.
+    """
 
-	defaultURI = 'tcp://firehose.elite-market-data.net:9050'
+    defaultURI = 'tcp://firehose.elite-market-data.net:9050'
 
-	def __init__(self, uri=None, ctx=None):
-		self.__uri = uri or Firehose.defaultURI
+    def __init__(self, uri=None, ctx=None):
+        self.__uri = uri or Firehose.defaultURI
 
-		if self.__uri.find("file://") == 0:
-			def _poll(timeout):
-				return 1
-			def _read():
-				result = self.__filehandle.readline().strip()
-				if not result: raise EOFError()
-				return result
+        if self.__uri.find("file://") == 0:
+            def _poll(timeout):
+                return 1
+            def _read(nonBlocking=False):
+                result = self.__filehandle.readline().strip()
+                if not result: raise EOFError()
+                return result
 
-			self.poll = _poll
-			self.read = _read
-			self.__filehandle = open(self.__uri[7:], 'r')
-		else:
-			def _poll(timeout):
-				return self.__socket.poll(timeout)
-			def _read():
-				result = self.__socket.recv_string()
-				if not result: raise EOFError()
-				return result
+            self.poll = _poll
+            self.read = _read
+            self.__filehandle = open(self.__uri[7:], 'r')
+        else:
+            def _poll(timeout):
+                #### TODO: Use a ZMQ Poller so we can catch ctrl-c
+                return self.__socket.poll(timeout)
+            def _read(nonBlocking=False):
+                flags = zmq.NOBLOCK if nonBlocking else 0
+                result = self.__socket.recv_string(flags)
+                if not result: raise EOFError()
+                return result
 
-			# All ZMQ operations are done through a Context,
-			# so use one we're given or create one for ourselves.
-			self.__ctx = ctx or zmq.Context()
+            # All ZMQ operations are done through a Context,
+            # so use one we're given or create one for ourselves.
+            self.__ctx = ctx or zmq.Context()
 
-			# EMDN is using the pub/sub model, a bit overzealously,
-			# so we need a subscriber socket subscribed to nothing.
-			self.__socket = self.__ctx.socket(zmq.SUB)
-			self.__socket.setsockopt(zmq.SUBSCRIBE, ''.encode())
-			self.__socket.connect(self.__uri)
-			self.poll = _poll
-			self.read = _read
+            # EMDN is using the pub/sub model, a bit overzealously,
+            # so we need a subscriber socket subscribed to nothing.
+            self.__socket = self.__ctx.socket(zmq.SUB)
+            self.__socket.setsockopt(zmq.SUBSCRIBE, ''.encode())
+            self.__socket.connect(self.__uri)
+            self.poll = _poll
+            self.read = _read
 
 
-	def drink(self, records=None, timeout=None, burst=False):
-		"""
-			Drink from the firehose, yielding the data we retrieve as ItemRecords.
+    def drink(self, records=None, timeout=None, burst=False):
+        """
+            Drink from the firehose, yielding the data we retrieve as ItemRecords.
 
-			Keyword arguments:
-			records -- maximum number of records to yield before exiting. Default, 0, is unlimited.
-			timeout -- maximum total time to wait for data.
-			burst   -- set True to stop after the first set of records are retrieved.
+            Keyword arguments:
+            records -- maximum number of records to yield before exiting. Default, 0, is unlimited.
+            timeout -- maximum total time to wait for data.
+            burst   -- set True to stop after the first set of records are retrieved.
 
-			e.g.
-			  drink(records=50, timeout=300)
-			    Reads until we have received 50 seconds or 300 seconds have passed.
+            e.g.
+              drink(records=50, timeout=300)
+                Reads until we have received 50 seconds or 300 seconds have passed.
 
-			  drink(records=50, timeout=10.5, burst=True)
-			    Reads until the first of:
-			      50 records have been received,
-			      10.5 seconds has elapsed,
-			      the first burst of data has been drained.
-		"""
+              drink(records=50, timeout=10.5, burst=True)
+                Reads until the first of:
+                  50 records have been received,
+                  10.5 seconds has elapsed,
+                  the first burst of data has been drained.
+        """
 
-		maxPollDuration = timeout
-		recordsRemaining = records or 1
-		recordCost = 1 if records else 0
+        maxPollDuration = timeout
+        recordsRemaining = records or -1
 
-		if timeout:
-			cutoffTime = time.clock() + timeout
+        cutoffTime = time.clock() + timeout if timeout else None
 
-		while recordsRemaining:
-			if timeout:
-				maxPollDuration = (cutoffTime - time.clock()) * 1000
-				if maxPollDuration <= 0:
-					return
-			if self.poll(timeout=maxPollDuration):
-				while recordsRemaining:
-					try:
-						csv = self.read()
-					except (zmq.error.Again, EOFError):
-						return
-					yield ItemRecord(*(csv.split(',')))
-					recordsRemaining -= recordCost
-				if burst:
-					return
+        while recordsRemaining:
+            if timeout:
+                maxPollDuration = int(((cutoffTime - time.clock()) * 1000) + 0.9999)
+                if maxPollDuration <= 0:
+                    return
+            pollResult = self.poll(timeout=maxPollDuration)
+            if pollResult:
+                while recordsRemaining:
+                    try:
+                        csv = self.read(nonBlocking=True)
+                    except EOFError:
+                        return
+                    except (zmq.error.Again, BlockingIOError, InterruptedError):
+                        break
+                    yield ItemRecord(*(csv.split(',')))
+                    recordsRemaining -= 1
+                if burst:
+                    return
