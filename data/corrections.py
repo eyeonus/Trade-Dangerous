e829b5844cb95654dd6fa6b625d9f4d6129d59eb
Oliver Smith <oliver@kfs.org>
2014-10-28 23:51:40 -0700

    Data changes in support of Beta 3
    - Corrections now supports Stars, Systems, Categories and Items,
    - Added some new items I saw in Beta 3,
    - Added some item and category changes I saw in beta 3,

--- data/corrections.py
+++ data/corrections.py
@@ -1,14 +1,44 @@
 # Provides an interface for correcting star/station names that
 # have changed in recent versions.
 
-corrections = {
+systems = {
+}
+
+stations = {
     'LOUIS DE LACAILLE PROSPECT': 'Lacaille Prospect',
     'HOPKINS HANGAR': 'Hopkins Hanger',
 }
 
-def correct(oldName):
+categories = {
+    'Drugs':            'Legal Drugs',
+}
+
+items = {
+    'HYDROGEN FUELS':   'Hydrogen Fuel',
+    'MARINE SUPPLIES':  'Marine Equipment',
+    'TERRAIN ENRICH SYS': 'Land Enrichment Systems',
+}
+
+def correctSystem(oldName):
+    try:
+        return systems[oldName.upper()]
+    except KeyError:
+        return oldName
+
+def correctStation(oldName):
 	try:
-		return corrections[oldName.upper()]
+		return stations[oldName.upper()]
 	except KeyError:
 		return oldName
 
+def correctCategory(oldName):
+    try:
+        return categories[oldName.upper()]
+    except KeyError:
+        return oldName
+
+def correctItem(oldName):
+    try:
+        return items[oldName.upper()]
+    except KeyError:
+        return oldName
