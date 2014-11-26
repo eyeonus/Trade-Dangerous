084e5dfd08e1447e2f5f4c991ff6b801b07020fb
Oliver Smith <oliver@kfs.org>
2014-11-25 22:53:59 -0800

    Problem with default ctor for TradeDB

--- tradedb.py
+++ tradedb.py
@@ -278,7 +278,7 @@ class TradeDB(object):
                     includeTrades=True,
                     debug=None,
                 ):
-        tdenv = tdenv or TradeEnv(debug)
+        tdenv = tdenv or TradeEnv(debug=debug)
         self.tdenv = tdenv
         self.dbPath = Path(tdenv.dbFilename or TradeDB.defaultDB)
         self.dbURI = str(self.dbPath)
