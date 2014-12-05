7740fc8a5239e5ed21b3c72f4f0983c28cc03f91
Oliver Smith <osmith@fb.com>
2014-12-04 23:05:03 -0800

    Added System.distToSq
    Calculates distance between two stars

--- tradedb.py
+++ tradedb.py
@@ -97,6 +97,17 @@ class System(object):
         self._rangeCache = None
 
 
+    def distToSq(self, other):
+        """
+            Calculate the square of the distance (in ly)
+            to a given other star.
+        """
+        dx2 = (self.posX - other.posX) ** 2
+        dy2 = (self.posY - other.posY) ** 2
+        dz2 = (self.posZ - other.posZ) ** 2
+        return (dx2 + dy2 + dz2)
+
+
     def name(self):
         return self.dbname.upper()
 
