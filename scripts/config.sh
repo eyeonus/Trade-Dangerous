de558cfcc21ca7be82a8163eb34792b03a9e0945
Oliver Smith <oliver@kfs.org>
2014-12-07 23:08:28 -0800

    Added bash scripts to make life easier

---
+++ scripts/config.sh
@@ -0,0 +1,34 @@
+### TradeDangerous script configuration
+# This file is just a bash script that sets several standard variables.
+# It is loaded by the various 'tdxxx' scripts.
+
+# How to launch trade dangerous
+TRADEPY="${TRADEDIR:-.}/trade.py"
+
+### COMMANDER/SHIP CONFIGURATION
+#
+
+# Put your name here for absolutely no reason at all, *yet*
+CMDR="YourName"
+
+# Current credit balance
+CR=270742
+
+# You can only have one set of variables "live" at a time, comment out
+# old ships while you are not flying them, then you can just uncomment
+# them later on.
+
+# Tweaked Hauler
+CAP=20
+EMPTYLY=16.53
+MAXLY=9.23
+JUMPS=6
+
+
+# Crappy Sidewinder
+#CAP=4
+#MAXLY=9.02
+#EMPTYLY=10.04
+#JUMPS=4
+#CR=10203
+
