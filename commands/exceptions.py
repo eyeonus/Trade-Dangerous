b2228b761b51e3ee4e520bdabf949b915c116c01
Oliver Smith <oliver@kfs.org>
2014-12-29 22:02:21 -0800

    Better explanation of run errors

--- commands/exceptions.py
+++ commands/exceptions.py
@@ -36,18 +36,17 @@ class NoDataError(TradeException):
     def __init__(self, errorStr):
         self.errorStr = errorStr
     def __str__(self):
-        return "Error: {}\n".format(self.errorStr) + (
-                "This can happen when there are no profitable trades"
-                " matching your criteria, or if you have not yet entered"
-                " any price data for the station(s) involved.\n"
-                "\n"
-                "See '{} update -h' for help entering/updating prices, or"
-                " obtain a '.prices' file from the web, such as maddavo's:"
-                " http://www.davek.com.au/td/\n"
-                "\n"
-                "See https://bitbucket.org/kfsone/tradedangerous/wiki/"
-                    "Price%20Data"
-                " for more help."
-            ).format(sys.argv[0])
+        return "Error: {}\n".format(self.errorStr) + ("""
+This can either indicate a lack of data (such as missing
+price information, station data, etc) or that there was no
+data matching your criteria.
+
+See '{} update -h' for help entering/updating prices, or
+obtain a crowd-sourced '.prices' file from the web, such
+as maddavo's (http://www.davek.com.au/td/)".
+
+For more help, see the TradeDangerous Wiki:
+    http://kfs.org/td/wiki
+""").format(sys.argv[0])
 
 
