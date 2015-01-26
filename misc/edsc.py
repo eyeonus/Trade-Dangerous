bb2a9d00aeb84f697af19e66fecfba0f42623f27
Oliver Smith <oliver@kfs.org>
2015-01-25 16:48:38 -0800

    Improved parsing of EDSC results

--- misc/edsc.py
+++ misc/edsc.py
@@ -2,11 +2,12 @@
 
 from __future__ import absolute_import, with_statement, print_function, division, unicode_literals
 
-from collections import defaultdict
+from collections import defaultdict, namedtuple
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 
 import json
+import os
 
 try:
     import requests
@@ -62,6 +63,228 @@ class StarQuery(object):
 
 class SubmissionError(Exception):
     pass
+
+
+class Status(namedtuple('Status', [
+        'source', 'code', 'msg', 'lhs', 'rhs'
+        ])):
+    pass
+
+class StarSubmissionResult(object):
+    """
+    Translates a response the json we get back from EDSC when
+    we submit a StarSubmission into something less awful to
+    work with.
+
+    Attributes:
+        valid
+            True or False whether the response looked healthy
+
+        summary
+            A summary of the /parse/; currently either "No Data"
+            or "OK". This does NOT represent whether the
+            submission itself was successful.
+
+        systems
+            A dict{ system name: [msgnum, coords] } for systems which
+            were reported as being added or updated. See
+            translateCode for converting these to text.
+            'coords' is either None if the system hasn't been
+            trilaterated yet, or an array of [x,y,z]
+
+        distances
+            A dict{ lhs system: { rhs system: dist } }. It's not
+            really very useful but I didn't want to drop the data
+            EDSC was sending back to us, just incase.
+
+        errors
+            A list of SubmissionError objects describing problems
+            that were encountered, mostly 304 (distance added but
+            failed to verify coordinates) and 305 (distance appears
+            to be wrong).
+
+        recheck
+            A dict{ system name: dist } for systems that had a 305
+            error for a distance to "star". They still appear in
+            the 'errors' attribute, but this is a short-cut for
+            prompting the user to double check a distance from their
+            current p0.
+
+    """
+
+    codeMap = {
+        201:        "New Entry",
+        202:        "System CR increased",
+        301:        "Distance added",
+        302:        "Distance CR increased",
+        303:        "Added verification",
+        304:        "OK: Needs more data",
+        305:        "Distance appears to be wrong",
+        401:        "CALCULATED",
+        402:        "No solution found, more data needed"
+    }
+
+
+    def __init__(self, star, response):
+        self.star = star
+        self.valid = False
+        self.summary = "No Data"
+        self.systems = {}
+        self.distances = {}
+        self.errors = []
+        self.recheck = {}
+
+        # This line righ there, this is just the beginning of
+        # why it's necessary to have this complicated of a
+        # wrapper for this json data. Don't forget, we have
+        # already peeled off some layers of json around this.
+        summary = response['status']['input'][0]['status']
+        code, msg = int(summary['statusnum']), summary['msg']
+        if code != 0:
+            self.valid = False
+            self.summary = "Error #{}: {}".format(code, msg)
+            self.errors.append(Status(
+                    'status', code, msg, None, None
+            ))
+            return
+
+        self.valid = True
+        self.summary = "OK"
+
+        # We get back an array of things called 'system', which
+        # allows EDSC to tell us that something got added. It
+        # could be p0 or it could be one of the refs or it could
+        # be something that was sitting in EDSCs distances buffer.
+        sysArray = response['status']['system']
+        for ent in sysArray:
+            sysName = ent['system'].upper()
+            code = int(ent['status']['statusnum'])
+            msg = ent['status']['msg']
+            if code in [201, 202]:
+                self.systems[sysName] = (code, None)
+            else:
+                self.errors.append(Status(
+                        'system', code, msg, sysName, None,
+                ))
+
+        # Now the pair-wise distance checks. These are either not
+        # very useful/duplicate the systems list, or they tell us
+        # about conflicts with pre-existing data.
+        # Pairs which generate 305 (distance wrong) that relate
+        # to P0 will be added to "recheck" so that the caller can
+        # ask their user to provide sanity values.
+        distArray = response['status']['dist']
+        errPairs = set()
+        for ent in distArray:
+            lhsName = ent['system1'].upper()
+            rhsName = ent['system2'].upper()
+            code = int(ent['status']['statusnum'])
+            msg = ent['status']['msg']
+            if code in [301, 302, 303, 304]:
+                if not lhsName in self.distances:
+                    self.distances[lhsName] = {}
+                try:
+                    rhsDists = self.distances[rhsName]
+                    if lhsName in rhsDists:
+                        continue
+                except KeyError:
+                    pass
+                dist = float(ent['dist'])
+                self.distances[lhsName][rhsName] = dist
+                if not lhsName in self.systems:
+                    self.systems[lhsName] = (code, None)
+            else:
+                if (lhsName,rhsName,code) in errPairs:
+                    continue
+                if (rhsName,lhsName,code) in errPairs:
+                    continue
+                errPairs.add((lhsName,rhsName,code))
+                errPairs.add((rhsName,lhsName,code))
+                self.errors.append(Status(
+                        'dist', code, msg, lhsName, rhsName,
+                ))
+                if code == 305:
+                    if lhsName == star:
+                        self.recheck[rhsName] = ent['dist']
+                    elif rhsName == star:
+                        self.recheck[lhsName] = ent['dist']
+
+        # Finally we look thru the trilat array which is telling us
+        # how the search for p0's coordinates went on a per-ref
+        # basis.
+        #
+        # Successful trilateration will result in an array of
+        # trilats that contain code 401 and the coordinates of
+        # p0, one per ref. Since we can't tell what the *other*
+        # system is, this is a bit stupid. Probably a bug in edsc.
+        triArray = response['status']['trilat']
+        for ent in triArray:
+            sysName = ent['system'].upper()
+            code = int(ent['status']['statusnum'])
+            if code == 401:
+                try:
+                    system = self.systems[sysName]
+                    assert system[1] is None
+                except KeyError:
+                    system = (code, None)
+                assert system[1] is None
+                coord = ent['coord']
+                x, y, z = coord['x'], coord['y'], coord['z']
+                self.systems[sysName] = (system[0], [x,y,z])
+            elif code == 402:
+                pass
+            else:
+                self.errors.append(Status(
+                        'trilat', code, msg, sysName, None,
+                ))
+
+
+    def __str__(self):
+        if not self.valid:
+            return "ERROR: {}".format(self.summary)
+
+        text = ""
+
+        if not self.errors:
+            text += "Success.\n"
+
+        if self.systems:
+            text += "+Updates:\n"
+            sysNames = list(self.systems.keys())
+            sysNames.sort(key=lambda s: self.systems[s][0])
+            for sysName in sysNames:
+                code, coords = self.systems[sysName]
+                sysText = self.translateCode(code)
+                if coords is not None:
+                    sysText += str(coords)
+                text += "|- {:.<30s} {}\n".format(
+                    sysName, sysText,
+                )
+            text += "\n"
+
+        if self.errors:
+            text += "+Problems:\n"
+            errors = sorted(self.errors, key=lambda e: e.rhs)
+            errors.sort(key=lambda e: e.lhs)
+            errors.sort(key=lambda e: e.code)
+            for err in errors:
+                text += "|- {:.<30s} #{} {}".format(
+                        err.lhs,
+                        err.code,
+                        self.translateCode(err.code),
+                )
+                if err.rhs:
+                    text += " <-> " + err.rhs
+                text += "\n"
+
+        return text
+
+    def translateCode(self, code):
+        try:
+            return self.codeMap[code]
+        except KeyError:
+            return "Error #{} (unknown)".format(code)
+
 
 
 class StarSubmission(object):
@@ -134,53 +357,13 @@ class StarSubmission(object):
 
         try:
             respData = json.loads(resp)
-            return respData['d']
         except Exception:
             raise SubmissionError("Invalid server response: " + resp)
-
-
-def annotate_submission_response(r):
-    if not isinstance(r, dict) or 'status' not in r:
-        print("Unknown EDSC response: {}".format(r))
-        return
-    status = r['status']
-    result = status['input'][0]['status']
-    if result['statusnum'] != 0:
-        print("*** {} (#{})".format(result['msg'], result['statusnum']))
-    systems = defaultdict(list)
-    for sysEnt in status['system']:
-        sysName = sysEnt['system'].upper()
-        systems[sysName] += [
-            "{} (#{})".format(
-                sysEnt['status']['msg'],
-                sysEnt['status']['statusnum'],
-            )
-        ]
-    for triEnt in status['trilat']:
-        sysName = triEnt['system'].upper()
-        triStatus = triEnt['status']
-        triStatusNum = triStatus['statusnum']
-        if triStatusNum == 401:
-            coord = triEnt['coord']
-            msg = "Position [{},{},{}]".format(
-                coord['x'], coord['y'], coord['z']
-            )
-        else:
-            msg = "{} (#{})".format(triStatus['msg'], triStatusNum)
-        if not msg in systems[sysName]:
-            systems[sysName].append(msg)
-    for sys, vals in systems.items():
-        print("{}: {}.".format(sys, '. '.join(vals)))
-    for distEnt in status['dist']:
-        lhsName = distEnt['system1'].upper()
-        rhsName = distEnt['system2'].upper()
-        print("{}->{}: {} (#{}). Dist={}".format(
-            lhsName, rhsName,
-            distEnt['status']['msg'],
-            distEnt['status']['statusnum'],
-            distEnt['dist'],
-
-        ))
+        try:
+            data = respData['d']
+            return data
+        except KeyError:
+            raise SubmissionError("Server Error: " + resp)
 
 
 if __name__ == "__main__":
