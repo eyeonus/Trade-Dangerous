c6f43c64434d3707c5c8199a133c5ba0f0731680
Oliver Smith <osmith@fb.com>
2015-03-03 14:26:55 -0800

    Made it easier to access first/last station properties of a Route object

--- tradecalc.py
+++ tradecalc.py
@@ -134,12 +134,33 @@ class Route(object):
     __slots__ = ('route', 'hops', 'startCr', 'gainCr', 'jumps', 'score')
 
     def __init__(self, stations, hops, startCr, gainCr, jumps, score):
+        assert stations
         self.route = stations
         self.hops = hops
         self.startCr = startCr
         self.gainCr = gainCr
         self.jumps = jumps
         self.score = score
+
+    @property
+    def firstStation(self):
+        """ Returns the first station in the route. """
+        return self.route[0]
+
+    @property
+    def firstSystem(self):
+        """ Returns the first system in the route. """
+        return self.route[0].system
+
+    @property
+    def lastStation(self):
+        """ Returns the last station in the route. """
+        return self.route[-1]
+
+    @property
+    def lastSystem(self):
+        """ Returns the last system in the route. """
+        return self.route[-1].system
 
     def plus(self, dst, hop, jumps, score):
         """
@@ -163,7 +184,7 @@ class Route(object):
         return self.score == rhs.score and len(self.jumps) == len(rhs.jumps)
 
     def str(self):
-        return "%s -> %s" % (self.route[0].name(), self.route[-1].name())
+        return "%s -> %s" % (self.firstStation.name(), self.lastStation.name())
 
     def detail(self, tdenv):
         """
@@ -172,7 +193,7 @@ class Route(object):
 
         detail, goalSystem = tdenv.detail, tdenv.goalSystem
 
-        credits = self.startCr
+        credits = self.startCr + (tdenv.insurance or 0)
         gainCr = 0
         route = self.route
 
@@ -317,11 +338,12 @@ class Route(object):
 
             gainCr += hopGainCr
 
-        if route[-1].system is not goalSystem:
-            text += goalDistance(route[-1])
+        lastStation = self.lastStation
+        if lastStation.system is not goalSystem:
+            text += goalDistance(lastStation)
         text += footer or ""
         text += endFmt.format(
-            station=decorateStation(route[-1]),
+            station=decorateStation(lastStation),
             gain=gainCr,
             credits=credits + gainCr
         )
@@ -780,7 +802,7 @@ class TradeCalc(object):
                     maxPadSize=maxPadSize,
                     maxLsFromStar=maxLsFromStar,
                 )
-         
+
         station_iterator = _get_destinations
 
         prog = pbar.Progress(len(routes), 25)
@@ -789,7 +811,7 @@ class TradeCalc(object):
                 prog.increment(1)
             tdenv.DEBUG1("Route = {}", route.str())
 
-            srcStation = route.route[-1]
+            srcStation = route.lastStation
             srcTradingWith = srcStation.tradingWith
             if srcStation.tradingWith is None:
                 srcTradingWith = srcStation.tradingWith = dict()
@@ -808,7 +830,7 @@ class TradeCalc(object):
                 pass
 
             if goalSystem:
-                origSystem = route.route[0].system
+                origSystem = route.firstSystem
                 srcSystem = srcStation.system
                 srcGoalDist = srcSystem.distanceTo(goalSystem)
                 srcOrigDist = srcSystem.distanceTo(origSystem)
