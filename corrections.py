94853bf5792ca3c3aa98ed9053379d0c9ba6e2ce
Oliver Smith <oliver@kfs.org>
2015-03-27 00:11:30 -0700

    New Yembo's Under Construction is now Unity

--- corrections.py
+++ corrections.py
@@ -100,6 +100,7 @@ stations = {
     "CHICK EK/WH IEELOCK GATEWAY":          DELETED,
     "LHS 3006/WCM TRANSFER ORBITAL":        "Leonard Nimoy Station",
     "LALANDE 4141/4A5O4O":                  "4A5O4D",
+    "NEW YEMBO/UNDER CONSTRUCTION":         DELETED,
 }
 
 categories = {
