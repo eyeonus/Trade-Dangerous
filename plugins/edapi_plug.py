81482d33eb773d6043c2c5fd029c8604df3466fc
Tyler <tyler@briareos.net>
2015-10-10 14:36:22 -0400

    Version bump.

--- plugins/edapi_plug.py
+++ plugins/edapi_plug.py
@@ -21,7 +21,7 @@ import sys
 import textwrap
 import time
 
-__version_info__ = ('3', '5', '0')
+__version_info__ = ('3', '5', '1')
 __version__ = '.'.join(__version_info__)
 
 # ----------------------------------------------------------------
