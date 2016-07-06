0929a43a1b222126a043698b791c104e44838d06
Bernd Gollesch <bgollesch@speed.at>
2016-07-06 23:17:39 +0200

    ups, missed dictionary TradeDB.itemByFDevID for new EDCD plugin.

--- tradedb.py
+++ tradedb.py
@@ -1892,7 +1892,7 @@ class TradeDB(object):
             SELECT item_id, name, category_id, avg_price, fdev_id
               FROM Item
         """
-        itemByID, itemByName = {}, {}
+        itemByID, itemByName, itemByFDevID = {}, {}, {}
         for ID, name, categoryID, avgPrice, fdevID in self.cur.execute(stmt):
             category = self.categoryByID[categoryID]
             item = Item(
@@ -1902,10 +1902,14 @@ class TradeDB(object):
             )
             itemByID[ID] = item
             itemByName[name] = item
+            if fdevID:
+                itemByFDevID[fdevID] = item
+
             category.items.append(item)
 
         self.itemByID = itemByID
         self.itemByName = itemByName
+        self.itemByFDevID = itemByFDevID
 
         self.tdenv.DEBUG1(
             "Loaded {:n} Items",
