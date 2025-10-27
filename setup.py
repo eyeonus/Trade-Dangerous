07f3248574f2ff8cb0d8eefa37a84a1aae5c6d2e
Stefan Morrell <38956076+Tromador@users.noreply.github.com>
2025-10-27 22:55:48 +0000

    Fix: Update setup.py with new package & Python Version
    Added 'tradedangerous.db' package.
    Adjusted Python version requirements and added support for Python 3.11 and 3.12.
    Added minimum required Python 3.10

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@
 # --------------------------------------------------------------------
 # Copyright (C) Oliver 'kfsone' Smith 2014 <oliver@kfs.org>:
 # Copyright (C) Bernd 'Gazelle' Gollesch 2016, 2017
-# Copyright (C) Jonathan 'eyeonus' Jones 2018, 2019
+# Copyright (C) Stefan 'Tromador' Morrell 2025
+# Copyright (C) Jonathan 'eyeonus' Jones 2018 - 2025
 #
 # You are free to use, redistribute, or even print and eat a copy of
 # this software so long as you include this copyright notice.
@@ -25,41 +26,63 @@ package = "tradedangerous"
 
 exec(open("tradedangerous/version.py").read())  # pylint: disable=W0122
 
-setup(name = package,
-        version = __version__,  # pylint: disable=E0602
-        install_requires = ["requests", "appJar", "ijson", "rich"],
-        setup_requires = ["pytest-runner"],
-        tests_require = ["pytest"],
-        packages = ['.', 'tradedangerous', 'tradedangerous.commands', 'tradedangerous.mfd', 'tradedangerous.mfd.saitek', 'tradedangerous.misc', 'tradedangerous.plugins'],
-        url = "https://github.com/eyeonus/Trade-Dangerous",
-        project_urls = {
-            "Bug Tracker": "https://github.com/eyeonus/Trade-Dangerous/issues",
-            "Documentation": "https://github.com/eyeonus/Trade-Dangerous/wiki",
-            "Source Code": "https://github.com/eyeonus/Trade-Dangerous",
-        },
-        author = "eyeonus",
-        author_email = "eyeonus@gmail.com",
-        description = "Trade-Dangerous is a set of powerful trading tools for Elite Dangerous, organized around one of the most powerful trade run optimizers available.",
-        long_description = long_description,
-        long_description_content_type = "text/markdown",
-        keywords = ["trade", "elite", "elite-dangerous"],
-        classifiers = [
-            "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.7",
-            "Programming Language :: Python :: 3.8",
-            "Programming Language :: Python :: 3.9",
-            "Programming Language :: Python :: 3.10",
-            "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
-            "Operating System :: OS Independent",
-        ],
-        license = "MPL",
-        test_suite = "tests",
-        package_data = {"tradedangerous": ["templates/TradeDangerous.sql", "templates/Added.csv", "templates/Category.csv", "templates/RareItem.csv", "templates/database_changes.json"]},
-        entry_points = {
-            "console_scripts": [
-                "trade=trade:main",
-                "tradegui=tradegui:main"
-            ]
-        },
-        zip_safe = False
+setup(
+    name=package,
+    version=__version__,  # pylint: disable=E0602
+    install_requires=["requests", "appJar", "ijson", "rich"],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+    packages=[
+        ".",
+        "tradedangerous",
+        "tradedangerous.commands",
+        "tradedangerous.mfd",
+        "tradedangerous.mfd.saitek",
+        "tradedangerous.misc",
+        "tradedangerous.plugins",
+        "tradedangerous.db",
+    ],
+    url="https://github.com/eyeonus/Trade-Dangerous",
+    project_urls={
+        "Bug Tracker": "https://github.com/eyeonus/Trade-Dangerous/issues",
+        "Documentation": "https://github.com/eyeonus/Trade-Dangerous/wiki",
+        "Source Code": "https://github.com/eyeonus/Trade-Dangerous",
+    },
+    author="eyeonus",
+    author_email="eyeonus@gmail.com",
+    description=(
+        "Trade-Dangerous is a set of powerful trading tools for Elite Dangerous, "
+        "organized around one of the most powerful trade run optimizers available."
+    ),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=["trade", "elite", "elite-dangerous"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Operating System :: OS Independent",
+    ],
+    license="MPL",
+    python_requires=">=3.10",
+    test_suite="tests",
+    package_data={
+        "tradedangerous": [
+            "templates/TradeDangerous.sql",
+            "templates/Added.csv",
+            "templates/Category.csv",
+            "templates/RareItem.csv",
+            "templates/database_changes.json",
+        ]
+    },
+    entry_points={
+        "console_scripts": [
+            "trade=trade:main",
+            "tradegui=tradegui:main",
+        ]
+    },
+    zip_safe=False,
 )
